8e0c2b0e252d3732d134d7d3845827b880dc35bb
Rick Vang <MacGriddles@Ricks-MacBook-Pro.local>
2016-02-03 19:07:34 -0700

    moved project into project folders

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,15 @@
 var gulp = require('gulp')
 var sass = require('gulp-ruby-sass')
+var sass = require('gulp.sass');
 var connect = require('gulp-connect')
 gulp.task('connect', function(){
     connect.server({
         root:'public',
         port:4000
     })
-})
+});
+gulp.task('sass', function () {
+	gulp.src('./scss/*.scss')
+		.pipe(sass())
+		.pipe(gulp.dest('public/assets/styles'));
+});
